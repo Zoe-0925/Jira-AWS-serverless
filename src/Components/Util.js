270046290a73314d4637220882ffb179082d6f3e
Zoe Zhang <jin0925aki@gmail.com>
2020-11-04 14:15:47 +1100

    Update NavBar Create Project

--- src/Components/Util.js
+++ src/Components/Util.js
@@ -1,13 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
 
-export const initiateProjectAndStatus = (project, currentUserId) => {
+export const initiateProjectAndStatus = (projectData, currentUserId) => {
     const projectId = uuidv4()
     const statusIds = [uuidv4(), uuidv4(), uuidv4(), uuidv4()]
     const statusTemplate = addCreateAndUpdateDate({
         project: projectId, issues: []
     })
     const project = {
-        ...addCreateAndUpdateDate(project), _id: projectId, statusOrder: [statusIds],
+        ...addCreateAndUpdateDate(projectData), _id: projectId, statusOrder: [statusIds],
         lead: currentUserId
     }
     const status = [{ ...statusTemplate, _id: statusIds[0], name: "TO DO" },
