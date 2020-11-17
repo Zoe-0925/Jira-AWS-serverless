22b3d931df07a7bfb6ce4be7f7f6f00edac575c5
Zoe Zhang <jin0925aki@gmail.com>
2020-11-17 15:38:27 +1100

    Added Create Status

--- src/Components/DragDrop/StatusCreate.js
+++ src/Components/DragDrop/StatusCreate.js
@@ -1,31 +1,42 @@
-import React,{useState} from 'react'
-import Column from "./Column"
+import React, { useState } from 'react'
 import AddBoxRoundedIcon from '@material-ui/icons/AddBoxRounded';
 import { useSelector, useDispatch } from "react-redux"
-import { createStatus } from "../../Actions/status.actions"
+import { chainCreateStatus } from "../../Actions/status.actions"
 import { selectCurrentProjectId } from "../../Reducers/Selectors"
+import { v4 as uuidv4 } from 'uuid'
 
-export default function StatusCreate() {
-    //TODO
-    //after editting, create a new status
-    const [showNewEditable, setShowEditable] = useState(false)
+export function EmptyColumn({ hide }) {
     const dispatch = useDispatch()
+    const { state, setState, edit, setEdit } = useEditText("")
+    const loading = useSelector(selectLoadingReducer).loading
     const currentProjectId = useSelector(selectCurrentProjectId)
 
-    const createNewColumn = (statusName) => {
-        const status = {
-            name: statusName,
-            project: currentProjectId,
-            issues: []
-        }
-        dispatch(createStatus(status))
+    const handleSubmit = () => {
+        dispatch(chainCreateStatus({
+            _id: uuidv4(), name: state.value, project: currentProjectId, issues: []
+        }))
+        hide()
     }
+
+    return (<div className="epic-box">
+        <div className="flex-row epic-title" id={status !== undefined ? status._id : ""}>
+            <EditableText name="epic-summary" className="epic-summary"
+                edit={edit} text={state.value || status.name} setEdit={setEdit}>
+                <Input name="status-title-input" state={state} setState={setState} setEdit={setEdit} handleSubmit={handleSubmit} ƒ />
+            </EditableText>
+        </div>
+        {loading && <CircularProgress className="editable-input" />}
+    </div>)
+}
+
+
+export default function StatusCreate() {
+    const [showNewEditable, setShowEditable] = useState(false)
 
     return (
         <div className="add-column-icon">
             {!showNewEditable && <AddBoxRoundedIcon onClick={() => setShowEditable(true)} />}
-            {showNewEditable && <Column initialStatus={{ name: "", project: "", issues: [] }} />}
+            {showNewEditable && <EmptyColumn hide={() => setShowEditable(false)} />}
         </div>
-    )   //TODO
-    //Add a submit editable to the column title.....
+    ) 
 }
