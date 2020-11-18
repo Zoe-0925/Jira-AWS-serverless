859f3e4d124c44909074dccf413038384d371bbf
Zoe Zhang <jin0925aki@gmail.com>
2020-11-19 10:00:29 +1100

    Added Issue Detail Skeleton

--- src/Components/Issues/IssueUpdate.js
+++ src/Components/Issues/IssueUpdate.js
@@ -1,8 +1,8 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { useSelector, useDispatch } from "react-redux"
 import { IssueSummaryInput, IssueDescriptionInput } from "./IssueInputs"
 import IssueAddEpic from "./IssueAddEpic"
-import {  Row, Col } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import Select from 'react-select';
 import {
     Divider,
@@ -20,6 +20,57 @@ import CommentHOC from "../Comment/CommentHOC"
 import CloseIcon from '@material-ui/icons/Close';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import { formatDate } from "../Util"
+import Skeleton from '@material-ui/lab/Skeleton';
+
+const IssueDetailSkeleton = ({ handleClose }) => <div>
+    <MuiDialogTitle disableTypography className="title">
+        <Row>
+            <Col>
+                <Breadcrumbs aria-label="breadcrumb" style={{ display: "inline" }}>
+                    Issue
+                </Breadcrumbs>
+            </Col>
+            <Col md="auto"></Col>
+            <Col xs lg="1">
+                <IconButton aria-label="close" className="close-btn" onClick={handleClose}>
+                    <CloseIcon />
+                </IconButton>
+            </Col>
+        </Row>
+    </MuiDialogTitle>
+    <div className="issue-detail-form">
+        <Row>
+            <Col sm="12" md="7">
+                <Row>
+                    <div className="left-container">
+                        <Skeleton variant="rect" animation="wave" width="inherit" height={50} style={{ marginBottom: "0.5rem" }} />
+                        <Skeleton variant="rect" animation="wave" width="70%" height={50} style={{ marginBottom: "3rem" }} />
+                        <Skeleton variant="rect" animation="wave" width="80%" height={30} style={{ marginBottom: "0.5rem" }} />
+                        <Skeleton variant="rect" animation="wave" width="inherit" height={30} style={{ marginBottom: "0.5rem" }} />
+                        <Skeleton variant="rect" animation="wave" width="60%" height={30} style={{ marginBottom: "0.5rem" }} />
+                        <br />
+                        <br />
+                        <br />
+                        <Skeleton variant="rect" animation="wave" width="40%" height={30} style={{ marginBottom: "2rem" }} />
+                        <Skeleton variant="rect" animation="wave" width="inherit" height={80} style={{ marginBottom: "2rem" }} />
+
+                    </div>
+                </Row>
+                <Row>
+                </Row>
+            </Col>
+            <Col sm="12" md="5">
+                <Skeleton variant="rect" animation="wave" width="50%" height={20} style={{ marginBottom: "1rem" }} />
+                <Skeleton variant="rect" animation="wave" width="95%" height={50} style={{ marginBottom: "2rem" }} />
+                <Skeleton variant="rect" animation="wave" width="70%" height={20} style={{ marginBottom: "1rem" }} />
+                <Skeleton variant="rect" animation="wave" width="80%" height={50} style={{ marginBottom: "2rem" }} />
+                <Skeleton variant="rect" animation="wave" width="60%" height={20} style={{ marginBottom: "1rem" }} />
+                <Skeleton variant="rect" animation="wave" width="90%" height={50} style={{ marginBottom: "2rem" }} />
+            </Col>
+        </Row>
+    </div>
+</div>
+
 
 const IssueDetail = ({ issue, open, handleClose }) => {
 
@@ -32,7 +83,8 @@ const IssueDetail = ({ issue, open, handleClose }) => {
             fullWidth={true}
             className="dialog-container"
         >
-            <IssueDetailForm issue={issue} handleClose={handleClose} />
+            {(issue === undefined || issue === "") && <IssueDetailSkeleton handleClose={handleClose} />}
+            {issue !== "" && <IssueDetailForm issue={issue} handleClose={handleClose} />}
         </Dialog>
     </Fragment>)
 }
