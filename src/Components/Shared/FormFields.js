a8a879ad7c19e290b2b4b63b6aa49a6f0495bf30
Zoe Zhang <jin0925aki@gmail.com>
2021-02-04 10:55:24 +1100

    Fixed the Responsive Create Project Modal

--- src/Components/Shared/FormFields.js
+++ src/Components/Shared/FormFields.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Select from 'react-select';
 import { InputLabel, TextareaAutosize } from '@material-ui/core';
 import { Field } from 'formik';
