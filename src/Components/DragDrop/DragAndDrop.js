d0d7288fc8b84ed55e70138c37cdc02f0f441637
Zoe Zhang <jin0925aki@gmail.com>
2021-02-17 21:51:20 +1100

    Updated Drag and Drop

--- src/Components/DragDrop/DragAndDrop.js
+++ src/Components/DragDrop/DragAndDrop.js
@@ -1,34 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux"
 import { MyDraggable, MyDroppable } from "./DraggableAndDroppable";
-import StatusCreate from "../StatusColumn/StatusCreateTab"
+import CreateStatusTab from "../StatusColumn/CreateStatusTab"
 import Column from "../StatusColumn/Column"
-import Skeleton from '@material-ui/lab/Skeleton';
 import { v4 as uuidv4 } from 'uuid'
-import IssueCard from "../Issues/IssueCard";
-import { selectAllStatusInArrayWithIssues, selectLoading } from "../../Reducers/Selectors"
+import IssueCard from "../IssueCard/IssueCard";
+import { selectStatusWithIssue, selectLoading } from "../../Reducers/Selectors"
 
 const DragAndDrop = () => {
-    const columns = useSelector(selectAllStatusInArrayWithIssues) //TODO undefined
+    let columns = useSelector(selectStatusWithIssue)
     const loading = useSelector(selectLoading)
-
-    
 
     return (
         <div className="epic-list">
-            {columns && columns.map((el, ind) => {
-                if (!loading) {
-                    return <MyDroppable key={ind} el={el} ind={ind}>
-                        <Column key={uuidv4()} initialStatus={el}>
-                            {el && el.issues && el.issues.map((issueId, index) => <MyDraggable key={uuidv4()} id={issueId} index={index}>
-                                <IssueCard key={uuidv4()} issueId={issueId} />
-                            </MyDraggable>)}
-                        </Column>
-                    </MyDroppable>
-                }
-                return <Skeleton key={uuidv4()} variant="rect" animation="wave" width={230} height={240} style={{ marginRight: "1rem" }} />
-            })}
-            <StatusCreate />
+            {columns.map((el, ind) => <MyDroppable key={ind} el={el} ind={ind}>
+                <Column key={uuidv4()} status={el}>
+                    {el.issues.map((issue, index) => <MyDraggable key={uuidv4()} id={issue._id} index={index}>
+                        <IssueCard key={uuidv4()} issue={issue} />
+                    </MyDraggable>
+                    )}
+                </Column>
+            </MyDroppable>
+            )}
+            <CreateStatusTab />
         </div>
     )
 }
