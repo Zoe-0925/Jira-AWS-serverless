052e41367d777a963a3419bd3819fd8294d22356
Zoe Zhang <jin0925aki@gmail.com>
2021-02-17 21:49:32 +1100

    Updated Drawer

--- src/Components/Drawer/Drawer.js
+++ src/Components/Drawer/Drawer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useSelector } from "react-redux"
 import { selectCurrentProjectName } from "../../Reducers/Selectors"
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { Drawer, IconButton, Hidden } from '@material-ui/core';
+import { Drawer, IconButton } from '@material-ui/core';
 import { ProjectHeaderTab } from "../Shared/Tabs"
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import CloseIcon from '@material-ui/icons/Close';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const drawerWidth = 240;
 
@@ -15,10 +16,8 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
     },
     drawer: {
-        [theme.breakpoints.up('sm')]: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
+        width: drawerWidth,
+        flexShrink: 0
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -56,48 +55,48 @@ export default function SideDrawer({ handleClick, open, ...props }) {
     </IconButton>)
 
     return <div className="drawer">
+        <CssBaseline />
         <nav className={classes.drawer}>
             {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-            <Hidden smUp implementation="css">
-                <Drawer
-                    className="mobile-drawer"
-                    variant="temporary"
-                    anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-                    open={open}
-                    onClose={closeDrawer}
-                    classes={{
-                        paper: classes.drawerPaper,
-                    }}
-                    ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                    }}
-                >
-                    {closeButton}
-                    <div className="title">
-                        <ProjectHeaderTab title={title || ""} subtite="Software project" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2sa8Zxht8_5o8aMA9I1rHmr9FVXEoxoDVfw&usqp=CAU" />
-                    </div>
-                    {props.children}
-                </Drawer>
-            </Hidden>
-            <Hidden xsDown implementation="css">
-                <Drawer
-                    className={classes.drawer + " desktop-drawer"}
-                    variant="persistent"
-                    open={open}
-                    classes={{
-                        paper: classes.drawerPaper,
-                    }}
-                >
-                    <div className={classes.toolbar} />
-                    <div className="title">
-                        <ProjectHeaderTab title={title || ""} subtite="Software project" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2sa8Zxht8_5o8aMA9I1rHmr9FVXEoxoDVfw&usqp=CAU" />
-                        <IconButton className="close-drawer-icon" onClick={open ? () => { handleClick(false) } : () => { handleClick(true) }}>
-                            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                        </IconButton>
-                    </div>
-                    {props.children}
-                </Drawer>
-            </Hidden>
+            <Drawer
+                className={classes.drawer + " desktop"}
+                variant="persistent"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.toolbar} />
+                <div className="title">
+                    <ProjectHeaderTab title={title || ""} subtite="Software project" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2sa8Zxht8_5o8aMA9I1rHmr9FVXEoxoDVfw&usqp=CAU" />
+                    <IconButton className="close-drawer-icon" onClick={open ? () => { handleClick(false) } : () => { handleClick(true) }}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                {props.children}
+            </Drawer>
+            <Drawer
+                className="mobile-drawer"
+                variant="temporary"
+                anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                open={open}
+                onClose={closeDrawer}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+                ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                }}
+            >
+                {closeButton}
+                <div className="title">
+                    <ProjectHeaderTab title={title || ""} subtite="Software project" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2sa8Zxht8_5o8aMA9I1rHmr9FVXEoxoDVfw&usqp=CAU" />
+                </div>
+                {props.children}
+            </Drawer>
+
+
+
         </nav>
     </div>
 }
