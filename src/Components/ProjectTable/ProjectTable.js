09ff9c2dac664b4d970e6a32b181eed24a51196f
Zoe Zhang <jin0925aki@gmail.com>
2021-02-18 12:46:31 +1100

    Fixed eslint warnings

--- src/Components/ProjectTable/ProjectTable.js
+++ src/Components/ProjectTable/ProjectTable.js
@@ -9,7 +9,7 @@ import { useDotIconMenu } from "../Shared/CustomHooks"
 import Skeleton from '@material-ui/lab/Skeleton';
 import { v4 as uuidv4 } from 'uuid'
 
-const ProjectTable = ({ loading, projects = [], users = [], goToBoardPage, goToProjectDetail, tableHeader = [], deleteProject }) => {
+const ProjectTable = ({ loading, projects, users, goToBoardPage, goToProjectDetail, tableHeader, deleteProject }) => {
     const { anchorEl, isOpen, anchorRef, handleMenuClose, handleMenuOpen } = useDotIconMenu()
 
     return (
@@ -18,17 +18,17 @@ const ProjectTable = ({ loading, projects = [], users = [], goToBoardPage, goToP
                 <Table className="project-list-table" aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            {tableHeader.map(each => <TableCell key={each} align="left">{each}</TableCell>)}
+                            {tableHeader && tableHeader.map(each => <TableCell key={each} align="left">{each}</TableCell>)}
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {projects.map(project => (
+                        {projects && projects.map(project => (
                             <TableRow key={project._id} className="table-body">
                                 <TableCell align="left" onClick={goToBoardPage}>{project.name}</TableCell>
                                 {[project.key, "Software"].map(each => <TableCell key={each} align="left">{each}</TableCell>)}
                                 <TableCell component="th" scope="row">
                                     <AccountCircleIcon />
-                                    {users.find(user => user._id === project.lead) ? users.find(user => user._id === project.lead).name : ""}</TableCell>
+                                    {users && users.find(user => user._id === project.lead) ? users.find(user => user._id === project.lead).name : ""}</TableCell>
                                 <TableCell component="th" scope="row" >
                                     <DotIconMenu className="dot-icon" anchorEl={anchorEl} isOpen={isOpen} anchorRef={anchorRef}
                                         handleMenuClose={handleMenuClose} handleMenuOpen={handleMenuOpen} >
